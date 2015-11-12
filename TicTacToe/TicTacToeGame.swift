aed8c6f15d73d9598e8435f27ef1b0b8029f969f
Chris Tran <christopher.huytran@gmail.com>
2015-11-11 23:22:46 -0800

    Add TicTacToeGame Model

---
+++ TicTacToe/TicTacToeGame.swift
@@ -0,0 +1,84 @@
+//
+//  TicTacToeGame.swift
+//  TicTacToe
+//
+//  Created by Huy Tran on 11/11/15.
+//  Copyright © 2015 Chris Tran. All rights reserved.
+//
+
+import Foundation
+
+class TicTacToeGame {
+    var gameBoard: [Int]
+    var gameOver: Bool
+    var winner: String?
+    var currentTurn: String
+    
+    init() {
+        gameBoard = [0,0,0,
+                     0,0,0,
+                     0,0,0]
+        gameOver = false
+        currentTurn = "Red"
+    }
+    
+    func updateGameBoardAt(index: Int, withColor: String) {
+        if withColor == "Red" {
+            gameBoard[index] = 1
+        } else {
+            gameBoard[index] = 2
+        }
+    }
+    
+    func resetGame() {
+        gameBoard = [0,0,0,
+            0,0,0,
+            0,0,0]
+        gameOver = false
+    }
+    
+    func checkGame() {
+        checkRows()
+        checkColumns()
+        checkDiagonals()
+    }
+    
+    func checkRows() {
+        for var i = 0; i <= 6; i += 3 {
+            if gameBoard[i] != 0 && gameBoard[i] == gameBoard[i+1] && gameBoard[i+1] == gameBoard[i+2] {
+                declareWinner(gameBoard[i])
+            }
+        }
+    }
+    
+    func checkColumns() {
+        for var i = 0; i < 3; i++ {
+            if gameBoard[i] != 0 && gameBoard[i] == gameBoard[i+3] && gameBoard[i+3] == gameBoard[i+6] {
+                declareWinner(gameBoard[i])
+            }
+        }
+    }
+    
+    func checkDiagonals() {
+        if gameBoard[4] != 0 && (leftDiagonal() || rightDiagonal()) {
+            declareWinner(gameBoard[4])
+        }
+    }
+    
+    func leftDiagonal() -> Bool {
+        return gameBoard[0] == gameBoard[4] && gameBoard[4] == gameBoard[8]
+    }
+    
+    func rightDiagonal() -> Bool {
+        return gameBoard[2] == gameBoard[4] && gameBoard[4] == gameBoard[6]
+    }
+    
+    func declareWinner(winningIdx: Int) {
+        gameOver = true
+        if winningIdx == 1 {
+            winner = "Red"
+        } else {
+            winner = "Blue"
+        }
+    }
+}
